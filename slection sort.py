01510ae28db93f8d020f6c368cc40a9e25fe6976
sanoy-si <119401357+sanoy-si@users.noreply.github.com>
2023-06-18 11:33:36 +0300

    Create slection sort.py

---
+++ slection sort.py
@@ -0,0 +1,14 @@
+class Solution: 
+    def select(self, arr, i):
+        n=len(arr)
+        mi=i
+        for j in range(i+1,n):
+            if arr[j]<arr[mi]:mi=j
+        return mi
+    
+    def selectionSort(self, arr,n):
+        for i in range(n):
+            mi=self.select(arr,i)
+            
+            arr[i],arr[mi]=arr[mi],arr[i]
+        return arr
