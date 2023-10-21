3f82f71655c44bb9f5cdf9d208ce5acf09740906
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2023-10-21 13:48:07 +0300

    Time: 297 ms (59.69%), Space: 16.7 MB (27.96%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
+        left = 0
+        ma = 0
+        cnt = 0
+        # first by True 
+        for right in range(len(answerKey)):
+            while cnt > k:
+                if answerKey[left] == 'F':
+                    cnt -= 1
+                left += 1
+            if answerKey[right] == 'F':
+                cnt += 1
+            if cnt <= k:
+                ma = max(ma,right - left + 1)
+
+        left = 0
+        cnt = 0
+        # Then by left
+        for right in range(len(answerKey)):
+            while cnt > k:
+                if answerKey[left] == 'T':
+                    cnt -= 1
+                left += 1
+            if answerKey[right] == 'T':
+                cnt += 1
+            if cnt <= k:
+                ma = max(ma,right - left + 1)
+
+        return ma
+                
