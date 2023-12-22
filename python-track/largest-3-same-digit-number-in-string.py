5e9189c8b7ec3ee2c4cd1dd78f0d9f63deb8c925
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2023-12-22 12:22:28 +0300

    Added solution for Largest 3-Same-Digit Number in String

--- python-track/largest-3-same-digit-number-in-string.py
+++ python-track/largest-3-same-digit-number-in-string.py
@@ -1,25 +1,17 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        num = [int(i) for i in num]
-        d = defaultdict(int)
-        ans = [0,0,0]
-        
-        for i in range(3):
-            d[num[i]] += 1
-            
-        if len(d) == 1:
-            ans = list(d.keys())
-        
+        counter = defaultdict(int)
+        ans = ""
         left = 0
-        for right in range(3,len(num)):
-            d[num[right]] += 1
-            d[num[left]] -= 1
-            if d[num[left]] == 0:
-                d.pop(num[left])
-            left += 1
-            if len(d) == 1:
-                curr = list(d.keys())
-                ans = max(curr,ans, key = lambda x: 3 * x[0])
-        ans = [str(i) for i in ans]
-        return ''.join(ans)*3 if ans != ['0','0','0'] else ""
+        for right in range(len(num)):
+            counter[num[right]] += 1
+            while right - left + 1 > 3:
+                counter[num[left]] -= 1
+                if counter[num[left]] == 0:
+                    counter.pop(num[left])
+                left += 1
+            if right - left + 1 == 3 and len(counter) == 1:
+                if not ans or int(ans) < int(num[left:right+1]):
+                    ans = num[left:right+1]
         
+        return ans
