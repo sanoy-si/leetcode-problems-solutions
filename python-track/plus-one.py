a6f885292aced3d16243af329cd641bbe21f09e9
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-01-03 22:52:00 +0300

    Added solution for Plus One

--- python-track/plus-one.py
+++ python-track/plus-one.py
@@ -1,21 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        carry = 1
-        ptr = len(digits) - 1
-
-        while ptr >= 0:
-            digits[ptr] += carry
-            if digits[ptr] > 9:
-                carry = int(str(digits[ptr])[0])
-                digits[ptr] = int(str(digits[ptr])[1])
-                ptr -= 1
-            else:
-                carry = 0
-                break
-
-        if carry != 0:
-            digits.insert(0,carry)
-
-        return digits
-
+        digits = [str(i) for i in digits]
+        digits = int(''.join(digits)) + 1
+        
+        return [int(i) for i in str(digits)]
+        
+        
+        
         
