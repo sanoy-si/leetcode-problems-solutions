18ee88acf9037dd77bdd18c950d432f15b766dc6
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-01-03 18:06:29 +0300

    Added solution for Sort the People

--- python-track/sort-the-people.py
+++ python-track/sort-the-people.py
@@ -1,17 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        data = {heights[i]:names[i] for i in range(len(heights))}
         flag = False
         for i in range(len(heights)):
             for j in range(len(heights)-1):
                 if heights[j] < heights[j+1]:
                     heights[j],heights[j+1] = heights[j+1],heights[j]
+                    names[j],names[j+1] = names[j+1], names[j]
                     flag = True
             if not flag:
                 break
 
-        sorted_names=[data[height] for height in heights]
-        return sorted_names
+        return names
         
 
         
