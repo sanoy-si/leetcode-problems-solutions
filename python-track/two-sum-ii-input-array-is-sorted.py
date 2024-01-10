a9befb17f0dc13b9f2d32a7f75950548995d03cc
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-01-10 19:21:54 +0300

    Added solution for Two Sum II - Input Array Is Sorted

--- python-track/two-sum-ii-input-array-is-sorted.py
+++ python-track/two-sum-ii-input-array-is-sorted.py
@@ -2,9 +2,10 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         left, right = 0, len(numbers) - 1
         while left < right:
-            if numbers[left] + numbers[right] == target:
+            current_sum = numbers[left] + numbers[right]
+            if  current_sum == target:
                 return [left + 1, right + 1]
-            elif numbers[left] + numbers[right] < target:
+            elif current_sum < target:
                 left += 1
             else:
                 right -= 1
