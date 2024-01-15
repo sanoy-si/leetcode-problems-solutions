7dfb8cb467a0b376256307984d6d35bb3baaafa6
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-01-15 11:03:56 +0300

    Added solution for Count Pairs Whose Sum is Less than Target

--- python-track/count-pairs-whose-sum-is-less-than-target.py
+++ python-track/count-pairs-whose-sum-is-less-than-target.py
@@ -1,10 +1,16 @@
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
         count = 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] < target:
-                    count += 1
+        nums.sort()
+        left = 0
+        right = len(nums) - 1 
         
-        return count
+        while left < right:
+            if nums[left] + nums[right] < target:
+                count += right - left 
+                left += 1
+            else:
+                right -= 1
+
+        return count  
         
