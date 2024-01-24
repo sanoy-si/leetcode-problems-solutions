d83a944f0ec3695af73e4de16be49c21ac74b093
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-01-24 18:05:52 +0300

    Added solution for Range Sum Query - Immutable

--- python-track/range-sum-query-immutable.py
+++ python-track/range-sum-query-immutable.py
@@ -1,13 +1,14 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
+        running_sum = 0
         self.prefix_sum = [0]
-        accumulator = 0
-
+        
         for num in nums:
-            accumulator += num
-            self.prefix_sum.append(accumulator)
+            running_sum += num
+            self.prefix_sum.append(running_sum)
+
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.prefix_sum[right + 1] - self.prefix_sum[left]
+       return self.prefix_sum[right + 1] - self.prefix_sum[left]
         
