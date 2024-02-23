0bb28864266fd41362b7808ca7ed1a093920b3d6
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-02-23 13:19:11 +0300

    Add solution for Rabbits in Forest

--- leetcode/rabbits-in-forest.py
+++ leetcode/rabbits-in-forest.py
@@ -2,6 +2,7 @@ class Solution:
     def numRabbits(self, answers: List[int]) -> int:
         answer = 0
         counter = Counter(answers)
+        
         for key, count in counter.items():
             colors_count = ceil(count / (key + 1))
             answer += colors_count * (key + 1)
