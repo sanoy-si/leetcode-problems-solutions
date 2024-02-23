ab5e68c181ae19c53c5cb4ba4eead0e8d5739197
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-02-23 18:19:40 +0300

    Add solution for Minimum Add to Make Parentheses Valid

---
+++ leetcode/minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
+
+        for char in s:
+            if stack and stack[-1] == '(' and char == ')':
+                stack.pop()
+
+            else:
+                stack.append(char)
+        
+        return len(stack)
+        
