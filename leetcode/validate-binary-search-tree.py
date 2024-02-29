adf9150af072a0e8493749fe2e12fbbe7e7da48f
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-02-29 10:50:05 +0300

    Add solution for Validate Binary Search Tree

--- leetcode/validate-binary-search-tree.py
+++ leetcode/validate-binary-search-tree.py
@@ -6,18 +6,33 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        self.list = []
-        
+
+        def is_valid(val, left_max, right_min):
+            if left_max == None:
+                left_max = float('-inf')
+
+            if right_min == None:
+                right_min = float('inf')
+
+            return left_max < val < right_min
+
         def dfs(node):
             if not node:
-                return 
-
-            dfs(node.left)
-
-            self.list.append(node.val)
+                return [True, None, None]
             
-            dfs(node.right)
-        
-        dfs(root)
+            left_is_valid, left_min, left_max = dfs(node.left)
+            right_is_valid, right_min, right_max = dfs(node.right)
 
-        return len(self.list) == len(set(self.list)) and sorted(self.list) == self.list 
+            if left_is_valid and right_is_valid and is_valid(node.val, left_max, right_min):
+                if left_min == None:
+                    left_min = node.val
+
+                if right_max == None:
+                    right_max = node.val
+
+                return [True, left_min, right_max]
+            
+            return [False, None, None]
+
+        return dfs(root)[0]
+            
