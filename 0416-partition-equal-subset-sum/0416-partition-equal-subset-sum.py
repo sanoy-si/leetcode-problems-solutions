b70f43ca8a7c61d595580245f60fe530cd7c020c
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-05-09 21:47:26 +0300

    Time: 1978 ms (14.18%), Space: 482.1 MB (6.4%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,7 +1,13 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        target = sum(nums ) / 2 
-        
+        total = sum(nums)
+
+        if total % 2 == 1:
+            return False
+
+        target = total / 2 
+
+        memory = {}
         def func(i, curr):
             if curr >= target:
                 return curr == target
@@ -9,6 +15,9 @@ class Solution:
             if i >= len(nums):
                 return False
             
-            return func(i + 1, curr) or func(i + 1, curr + nums[i])
-        
+            if (i, curr) not in memory:
+                memory[(i, curr)] = func(i + 1, curr) or func(i + 1, curr + nums[i])
+
+            return memory[(i, curr)]
+
         return func(0, 0)
