545abce7dbfc910f952e609516f2e7536dbff5c7
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-05-23 23:43:55 +0300

    Time: 56 ms (6.41%), Space: 17.3 MB (5.4%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -8,7 +8,7 @@ class Solution:
 
         for i in range(1, len(s) + 1):
             for j in range(1, len(t) + 1):
-                dp[i][j] = (dp[i][j - 1] or s[i - 1] == t[j - 1]) and dp[i - 1][j]  
+                dp[i][j] = (dp[i][j - 1] or s[i - 1] == t[j - 1]) and dp[i - 1][j - 1]  
         
         return dp[len(s)][len(t)]
                 
