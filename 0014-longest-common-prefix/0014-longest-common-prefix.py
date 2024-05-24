ca5d81696b0290163777181fbcd687e580ed1988
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-05-24 22:10:01 +0300

    Time: 47 ms (6.52%), Space: 16.6 MB (44.05%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,16 +1,81 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None for _ in range(26)]
+        self.is_end = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            idx = ord(char) - ord('a')
+
+            if not curr.children[idx]:
+                curr.children[idx] = TrieNode()
+            
+            curr = curr.children[idx]
+        
+        curr.is_end = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            idx = ord(char) - ord('a')
+            
+            if not curr.children[idx]:
+                return False
+            
+            curr = curr.children[idx]
+        
+        return curr.is_end
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for char in prefix:
+            idx = ord(char) - ord('a')
+            
+            if not curr.children[idx]:
+                return False
+        
+            curr = curr.children[idx]
+        
+        return True
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        ans = strs[0]
-        for s in strs[1:]:
-            l = min(len(ans),len(s))
-            s = s[:l]
-            ans = ans[:l]
-            
-            while s != ans and ans:
-                ans = ans[:-1]
-                s = s[:-1]
 
+        trie = Trie()
+
+        for word in strs:
+            trie.insert(word)
         
-        return ans
+        answer = []
+        stack = [trie.root]
+        while stack:
+            node = stack.pop()
+            count = 0
+            idx = 0
+            for i in range(26):
+                if node.children[i]:
+                    count += 1
+                    idx = i
+
+            if count != 1:
+                break
             
+            stack.append(node.children[idx])
+            answer.append(chr(97 + idx))
+
+        min_length = len(min(strs, key = lambda x: len(x)))
+
+        if answer:
+            return ''.join(answer[:min(len(answer), min_length)])
+
+        return ''
+
         
