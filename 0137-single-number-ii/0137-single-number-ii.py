faa1b9fc4d94ec202de7286dc5af31d0b93e5039
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-05-29 15:32:53 +0300

    Time: 81 ms (30.05%), Space: 18.6 MB (54.66%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        answer = 0
+        for i in range(31):
+            count = 0
+            for num in nums:
+                if num & 1 << i:
+                    count += 1
+            
+            if count % 3 != 0:
+                answer = answer | 1 << i
+        
+        if nums.count(answer) == 1:
+            return answer
+        
+        else:
+            ans = []
+            
+            for i in range(31):
+                if answer & 1 << i:
+                    ans.append('0')
+                
+                else:
+                    ans.append('1')
+
+            return -(int(''.join(ans[::-1]), 2) + 1)
