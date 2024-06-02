87aa7da4066ada4ef93d717c490eae2b7152478e
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-06-02 09:04:41 +0300

    Time: 170 ms (50.68%), Space: 20.9 MB (69.97%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,16 +1,8 @@
 class Solution:
-    def divide(self, s):
-        if len(s) == 1:return s
-        mid = len(s)//2
-        left = self.divide(s[0:mid])
-        right = self.divide(s[mid:])
-        return right + left
-
-    def reverseString(self, s: List[str]) -> None:
-        for i,j in enumerate(self.divide(s)):s[i] = j
-        
-        
-    
-    
+    def reverseString(self, s:List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
             
-        
