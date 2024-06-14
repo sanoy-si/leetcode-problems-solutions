fde0d4341ae3a59bd4635d3c181721da425a96b2
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-06-14 13:01:59 +0300

    Time: 104 ms (75.03%), Space: 17.9 MB (36.19%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.py
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -1,59 +1,65 @@
 class Node:
-    def __init__(self, val= -1, next = None):
-        self.val = val
-        self.next = next
+    def __init__(self,val=-1):
+        self.val=val
+        self.next=None
+        self.prev=None
 
 class MyLinkedList:
 
     def __init__(self):
-        self.head = Node()
-        
+        self.head=Node()
+        self.tail=Node()
+        self.tail.prev = self.head
+        self.head.next = self.tail
 
     def get(self, index: int) -> int:
-        curr = self.head
-        while index > -1 and curr:
-            curr = curr.next
-            index -= 1
-        
-        if index == -1 and curr:
+            curr=self.head
+
+            for i in range(index + 1):
+                if curr:
+                    curr=curr.next
+
+            if not curr or not curr.next:
+                return -1
+            
             return curr.val
         
-        return -1
-        
+
     def addAtHead(self, val: int) -> None:
         new = Node(val)
         new.next = self.head.next
-        self.head.next = new        
+        new.next.prev = new
+        new.prev = self.head
+        self.head.next = new
 
     def addAtTail(self, val: int) -> None:
         new = Node(val)
-        curr = self.head
-        while curr and curr.next:
-            curr = curr.next
-        
-        curr.next = new
-        
+        new.next = self.tail
+        new.prev = self.tail.prev
+        self.tail.prev.next = new
+        self.tail.prev = new
         
     def addAtIndex(self, index: int, val: int) -> None:
         new = Node(val)
+        
         curr = self.head
-        while index > 0 and curr:
-            curr = curr.next
-            index -= 1
+        for i in range(index):
+            if curr:
+                curr = curr.next
         
-        if index == 0 and curr:
-            if curr.next:
-                new.next = curr.next
-        
+        if curr and curr.next:
+            new.next = curr.next
+            new.next.prev = new
             curr.next = new
+            new.prev = curr
         
 
     def deleteAtIndex(self, index: int) -> None:
-        curr = self.head
-        while index > 0 and curr:
-            curr = curr.next
-            index -= 1
-        
-        if index == 0 and curr and curr.next:
-            curr.next = curr.next.next
-            
+            curr=self.head
+            for i in range(index):
+                if curr:
+                    curr=curr.next
+            if curr and curr.next and curr.next.val != -1:
+                curr.next=curr.next.next
+                curr.next.prev=curr
+       
