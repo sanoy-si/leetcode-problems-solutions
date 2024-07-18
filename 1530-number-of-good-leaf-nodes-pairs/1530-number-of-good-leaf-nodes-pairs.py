a266baa45e437b0385d0d1dca0e5673f0c4da00c
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-07-18 13:48:07 +0300

    Time: 83 ms (72.73%), Space: 17.1 MB (22.83%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -6,41 +6,9 @@
 #         self.right = right
 class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
-        answer = 0
-        def dfs(node, depth):
-            nonlocal answer
-
-            if not node:
-                return []
-
-            if not node.left and not node.right:
-                return [depth]
-            
-            left = dfs(node.left, depth + 1)
-            right = dfs(node.right, depth + 1)
-            
+        def merge_two_sorted_arrays(left, right):
+            p1 = p2 = 0
             merged_arr = []
-            p1 = p2 = 0
-
-            if left and right:
-                curr_distance = left[p1] - depth + right[p2] - depth
-                while curr_distance <= distance:
-                    left_val = left[p1]
-                    left_ans = 0
-                    while p1 < len(left) and left[p1] == left_val:
-                        left_ans += 1
-                        p1 += 1
-                    
-                    right_val = right[p2]
-                    right_ans = 0
-                    while p2 < len(right) and right[p2] == right_val:
-                        right_ans += 1
-                        p2 += 1
-
-                    answer += max(left_ans, right_ans)
-                    curr_distance = left[p1] - depth + right[p2] - depth if p1 < len(left) and p2 < len(right) else inf
-            
-            p1 = p2 = 0
             while p1 < len(left) and p2 < len(right):
                 
                 if left[p1] <= right[p2]:
@@ -59,7 +27,30 @@ class Solution:
 
             return merged_arr
 
-        
+        answer = 0
+        def dfs(node, depth):
+            nonlocal answer
+            if not node:
+                return []
+
+            if not node.left and not node.right:
+                return [depth]
+            
+            left = dfs(node.left, depth + 1)
+            right = dfs(node.right, depth + 1)
+            
+            p1, p2 = 0, len(right) - 1
+            while p1 < len(left) and p2 >= 0:
+                curr_distance = left[p1] - depth + right[p2] - depth
+                if curr_distance <= distance:
+                    answer += p2 + 1
+                    p1 += 1
+
+                else:
+                    p2 -= 1 
+            
+            return merge_two_sorted_arrays(left, right)
+            
         dfs(root, 0)
         return answer
 
