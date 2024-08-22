2d7bdafae4fe546c361c21c87dcb1dc4fae5566e
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-08-22 19:37:13 +0300

    Time: 315 ms (68.84%), Space: 18.9 MB (35.15%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.py
+++ 0664-strange-printer/0664-strange-printer.py
@@ -1,46 +1,14 @@
 class Solution:
     def strangePrinter(self, s: str) -> int:
-        counter = Counter()
-        n = len(s)
-        i = 0
-        while i < n:
-            curr = s[i]
-            while i < n and curr == s[i]:
-                i += 1
-            counter[curr] += 1
-        
-        chars = sorted('abcdefghijklmnopqrstuvwxyz', key = lambda x: counter[x], reverse = True)
-        idxs = defaultdict(list)
-        for i in range(n):
-            idxs[chars[i]].append(i)
-
-        answer = 0
-        added = set()
-        for char in chars:
-            if counter[char] == 0:
-                break
-            
-            i = 0
-            while i < n:
-                found = False
-                while i < n and char != s[i]:
-                    i += 1
-
-                while i < n and s[i] not in added:
-                    found = True
-                    i += 1
-
-                if found:
-                    answer += 1
-
-            added.add(char)
-
-        return answer
-
-            
-
-
-
-
-
-
+        ###
+        N = len(s)
+        @cache
+        def calc(left, right): 
+            if left >= right: 
+                return 0
+            best = calc(left + 1, right) + 1
+            for index in range(left + 1, right + 1): 
+                if s[left] == s[index]: 
+                    best = min(best, calc(left, index - 1) + calc(index, right))
+            return best
+        return calc(0, N - 1) + 1
