66729f91587af1c6aabf289750c698fa9fc688e4
Yonas Sintayehu <amyonassintayehu@gmail.com>
2024-10-09 09:35:10 +0300

    Time: 86 ms (80.03%), Space: 18.3 MB (82.87%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,24 +1,31 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         def top_sort(node):
+            if color[node] == 1:
+                return False
+
             color[node] = 1 
             for child in graph[node]:
-                if color[child] == 0:
-                    top_sort(child)
+                if color[child] != 2:
+                    if not top_sort(child):
+                        return False
             
             color[node] = 2
             order.append(node)
-            
+            return True
+
+
         order = []
         color = [0] * numCourses
-    
+        
         graph = [[] for _ in range(numCourses)]
         for course, pre in prerequisites:
             graph[pre].append(course)
         
         for node in range(numCourses):
             if color[node] == 0:
-                top_sort(node)
+                if not top_sort(node):
+                    return []
 
-        return order[::-1] if len(order) == numCourses else []
+        return order[::-1] 
         
