67eacfce136673fc1511fc113db9f383005991e5
Yonas Sintayehu <amyonassintayehu@gmail.com>
2024-10-13 10:47:44 +0300

    Time: 546 ms (99.86%), Space: 19.9 MB (96.52%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,53 +1,34 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        n = numCourses
-        graph =[[] for _ in range(n)]
-        for u, v in prerequisites:
-            graph[u].append(v)
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0 for _ in range(numCourses)]
 
-        def add_predecessors(node):
-            time_in, time_out = [-1] * n, [-1] * n
-            def dfs(start):
-                timer = 0 
-                color = [0] * n
-                stack = [start]
-                while stack:
-                    node = stack[-1]
-                    if color[node] == 0:
-                        time_in[node] = timer
-                        timer += 1
-                        color[node] = 1
+        for a, b in prerequisites:
+            graph[a].append(b)
+            indegree[b] += 1
 
-                        for child in graph[node]:
-                            if color[child] == 0:
-                                stack.append(child)  
-
-                    else:
-                        stack.pop()
-                        time_out[node] = timer
-                        timer += 1
-            
-            dfs(node)
-            for i in range(n):
-                if time_in[i] != -1 and time_in[node] < time_in[i] < time_out[i] < time_out[node]:
-                    is_ancestor[node][i] = True
+        prerequisites = [set() for _ in range(numCourses)]
         
-        is_ancestor = [[False for _ in range(n)] for _ in range(n)] 
-        for node in range(n):
-            add_predecessors(node)
-        
+
+        def bfs():
+            queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+
+            while queue:
+                node = queue.popleft()
+
+                for neighbour in graph[node]:
+                    indegree[neighbour] -= 1
+                    prerequisites[neighbour] |= prerequisites[node]
+                    prerequisites[neighbour].add(node)
+
+                    if indegree[neighbour] == 0:
+                        queue.append(neighbour) 
+                
+        bfs()
+
         answer = []
-        for ancestor, predecessor in queries:
-            answer.append(is_ancestor[ancestor][predecessor])
 
+        for a, b in queries:
+            answer.append(a in prerequisites[b])
+        
         return answer
-
-            
-
-
-
-
-        
-        
-
-        
