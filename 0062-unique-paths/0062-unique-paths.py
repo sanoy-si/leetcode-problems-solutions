9bb5dd7d39fb31bea80f14779fb89a3249186724
Yonas Sintayehu <amyonassintayehu@gmail.com>
2024-10-16 19:01:02 +0300

    Time: 34 ms (67.32%), Space: 16.5 MB (47.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,6 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        dp = [0] * (max(2, n + m - 1))
+        dp[0] = dp[1] = 1
+        for i in range(2, n + m - 1):
+            dp[i] = i * dp[i - 1]
+
         down_count = m - 1
         right_count = n - 1
 
-        return factorial(down_count + right_count) // (factorial(down_count) * factorial(right_count))
+        return dp[down_count + right_count] // (dp[down_count] * dp[right_count])
