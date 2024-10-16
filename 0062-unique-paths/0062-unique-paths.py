c97814c39a1603121f21d02e4c0de2dd0caeaabf
Yonas Sintayehu <amyonassintayehu@gmail.com>
2024-10-16 18:54:34 +0300

    Time: 37 ms (45.53%), Space: 16.6 MB (47.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,20 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memory = {}
+        down_count = m - 1
+        right_count = n - 1
 
-        inbound = lambda row, col: 0 <= row < m and 0 <= col < n
-
-
-        def dp(row, col):
-            if (row, col) == (m - 1, n - 1):
-                return 1
-            
-            if not inbound(row, col):
-                return False
-            
-            if (row, col) not in memory:
-                memory[(row, col)] = dp(row + 1, col) + dp(row, col + 1)
-            
-            return memory[(row, col)]
-        
-        return dp(0, 0)
+        return factorial(down_count + right_count) // (factorial(down_count) * factorial(right_count))
