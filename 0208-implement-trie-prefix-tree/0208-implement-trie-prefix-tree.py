c3a5c5cb88212a9996eea9ee5b3cada34473701c
Yonas Sintayehu <amyonassintayehu@gmail.com>
2025-11-03 14:28:40 +0300

    Time: 40 ms (76.69%), Space: 32.7 MB (23.44%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,51 +1,35 @@
 class TrieNode:
     def __init__(self):
-        self.children = [None for _ in range(26)]
-        self.is_end = False
+        self.children = defaultdict(TrieNode)
+        self.isEnd = False
 
 class Trie:
-
     def __init__(self):
         self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        curr = self.root
+        current = self.root
         for char in word:
-            idx = ord(char) - ord('a')
-
-            if not curr.children[idx]:
-                curr.children[idx] = TrieNode()
-            
-            curr = curr.children[idx]
-        
-        curr.is_end = True
+            current = current.children[char]
+        current.isEnd = True
 
     def search(self, word: str) -> bool:
-        curr = self.root
+        current = self.root
         for char in word:
-            idx = ord(char) - ord('a')
-            
-            if not curr.children[idx]:
+            current = current.children.get(char)
+            if current == None:
                 return False
-            
-            curr = curr.children[idx]
         
-        return curr.is_end
-        
+        return current.isEnd
 
     def startsWith(self, prefix: str) -> bool:
-        curr = self.root
-
+        current = self.root
         for char in prefix:
-            idx = ord(char) - ord('a')
-            
-            if not curr.children[idx]:
+            current = current.children.get(char)
+            if current == None:
                 return False
-        
-            curr = curr.children[idx]
-        
+            
         return True
-        
 
 
 # Your Trie object will be instantiated and called as such:
