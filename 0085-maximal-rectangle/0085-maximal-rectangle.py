d0e81da401e0bc879b327211430ec47b44611b77
Yonas Sintayehu <amyonassintayehu@gmail.com>
2026-01-11 14:54:42 +0300

    Time: 1076 ms (5.03%), Space: 24.5 MB (8.87%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,40 +1,22 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        def find_max_area(heights):
-            stack = []
-            min_range = {}
-            n = len(heights)
-            for i in range(n):
-                while stack and heights[stack[-1]] > heights[i]:
-                    idx = stack.pop()
-                    left = idx - stack[-1] - 1 if stack else idx
-                    right = i - idx - 1
-                    min_range[idx] = left + right + 1
-                stack.append(i)
-            
-            while stack:
-                idx = stack.pop()
-                left = idx - stack[-1] - 1 if stack else idx
-                right = n - idx - 1
-                min_range[idx] = left + right + 1
-            
-            answer = 0
-            for i in range(n):
-                answer = max(answer, heights[i] * min_range[i])
-
-            return answer
-        
-        n = len(matrix)
-        m = len(matrix[0])
-        heights = [0 for _ in range(m)]
-        answer = 0
-
+        n, m = len(matrix), len(matrix[0])
+        max_width = [[0 for _ in range(m)] for _ in range(n)]
         for i in range(n):
             for j in range(m):
-                heights[j] = heights[j] + 1 if matrix[i][j] == '1' else 0
-            answer = max(answer, find_max_area(heights))
+                if matrix[i][j] == '1':
+                    if j == 0:
+                        max_width[i][j] = 1 
+                    else:
+                        max_width[i][j] = max_width[i][j - 1] + 1
         
-      
-        
+        answer = 0
+        for j in range(m):
+            for bottom in range(n):
+                curr_width = inf
+                for top in range(bottom, -1, -1):
+                    curr_width = min(curr_width, max_width[top][j])
+                    answer = max(answer, curr_width * (bottom - top + 1))
+                
         return answer
-            
+  
